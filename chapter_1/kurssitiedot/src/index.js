4089eaf977025972c04b36469b6b90f0f72a2174
Laura Ojala <laojala@gmail.com>
2019-03-23 13:22:32 +0200

    1.4: kurssitiedot step4. Missing loops for data.

--- chapter_1/kurssitiedot/src/index.js
+++ chapter_1/kurssitiedot/src/index.js
@@ -3,55 +3,60 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Reactin perusteet',
+      exercises: 10
+    },
+    {
+      name: 'Tiedonvälitys propseilla',
+      exercises: 7
+    },
+    {
+      name: 'Komponenttien tila',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content names={[part1, part2, part3]} exc={[exercises1, exercises2, exercises3]} />
-      <Total total={[exercises1, exercises2, exercises3]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
+
+
 }
 
 const Header = (props) => {
   return (
-    <>
+    <div>
       <h1>{props.course}</h1>
-    </>
+    </div>
   )
 }
 
 const Content = (props) => {
   return (
     <>
-      <Part name={props.names[0]} exc={props.exc[0]}/>
-      <Part name={props.names[1]} exc={props.exc[1]}/>
-      <Part name={props.names[2]} exc={props.exc[2]}/>
+    <p>{props.parts[0].name + " " + props.parts[0].exercises}</p>
+    <p>{props.parts[1].name + " " + props.parts[1].exercises}</p>
+    <p>{props.parts[2].name + " " + props.parts[2].exercises}</p>
     </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exc}</p>
-    </div>
   )
 }
 
 const Total = (props) => {
   return (
     <>
-      <p>yhteensä: {props.total.reduce((a, b) => a + b, 0)}</p>
+    <p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }</p>
     </>
   )
 }
+
+
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
