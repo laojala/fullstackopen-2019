f3aa9fbcb6342be207d426b8448526ed2e73370c
Laura Ojala <laojala@gmail.com>
2019-03-31 10:52:19 +0300

    Excercise 1.6: unicafe step1

--- chapter_1/unicafe/src/index.js
+++ chapter_1/unicafe/src/index.js
@@ -6,7 +6,17 @@ const Button = (props) => (
       {props.text}
     </button>
   )
-  
+
+  const History = (props) => {
+    return (
+      <div>
+        <h1>Statistiikka</h1>
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+      </div>
+    )
+  }
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -14,11 +24,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = newValue => {
+    setGood(newValue)
+  }
+
+  const handleNeutral = newValue => {
+    setNeutral(newValue)
+  }
+
+  const handleBad = newValue => {
+    setBad(newValue)
+  }
+
   return (
     <div>
-      <Button handleClick={() => {} } text="Hyvä" />
-      <Button handleClick={() => {} } text="Neutraali" />
-      <Button handleClick={() => {}} text="Huono" />
+      <h1>Anna palautetta</h1>
+
+      <Button handleClick={() => handleGood(good+1)} text="hyvä" />
+      <Button handleClick={() => handleNeutral(neutral+1)} text="Neutraali" />
+      <Button handleClick={() => handleBad(bad+1)} text="Huono" />
+      <History good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
