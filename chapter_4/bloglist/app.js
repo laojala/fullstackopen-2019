6dc77f3d452809bed0cc0a975f85be001316b711
Laura Ojala <laojala@gmail.com>
2019-05-25 17:23:13 +0300

    add middleware for logging and error handling

--- chapter_4/bloglist/app.js
+++ chapter_4/bloglist/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
-//const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 
@@ -20,11 +20,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 app.use(express.static('build'))
 app.use(bodyParser.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
