782d3529564620059ae617759462d644df4acb1f
Laura Ojala <laojala@gmail.com>
2019-05-25 17:10:05 +0300

    refactor project structure according to the node.js practices

---
+++ chapter_4/bloglist/models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
