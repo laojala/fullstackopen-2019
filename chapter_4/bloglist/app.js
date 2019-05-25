8b9815102b45a5d42a62b5a1a624d1fab17d7950
Laura Ojala <laojala@gmail.com>
2019-05-25 17:46:42 +0300

    remove debug logging

--- chapter_4/bloglist/app.js
+++ chapter_4/bloglist/app.js
@@ -6,9 +6,6 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
-
-console.log('connecting to', config.MONGODB_URI)
-
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log('connected to MongoDB')
