7a7cf205b52d22c67e64bf8ad750ae11bc2a121f
Laura Ojala <laojala@gmail.com>
2019-05-28 07:36:29 +0300

    Add test for id. Move list tests to a different folder

--- chapter_4/bloglist/tests/blogs_api.test.js
+++ chapter_4/bloglist/tests/blogs_api.test.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
-// to run only this collection: bloglist laura$ npx jest tests/blogs_api.test.js --runInBand
+// to run only this collection:
+// npx jest tests/blogs_api.test.js --runInBand
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test.only('blog json contains field "id"', async () => {
+  await api
+  const blogs = await helper.blogsInDb()
+  console.log(blogs[0])
+  expect(blogs[0].id).toBeDefined()
 })
 
 afterAll(() => {
--- chapter_4/bloglist/tests/list_tests/list_helper.test.js
+++ chapter_4/bloglist/tests/list_tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require('../../utils/list_helper')
 const test_data = require('./test_data.js')
 
 test('dummy returns one', () => {
