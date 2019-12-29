30cf1c3e6efc30fee12729abcde0329548cc5711
Laura Ojala <laojala@gmail.com>
2019-12-29 07:28:19 +0200

    7.2: Route individual anecdote by id

--- chapter_7/routed-anecdotes/src/App.js
+++ chapter_7/routed-anecdotes/src/App.js
@@ -21,10 +21,26 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+
+  console.log(anecdote)
+  console.log("HEREE:", anecdote.content)
+  
+  return (
+  <div>
+    <h3>{anecdote.content}</h3>
+    <div>has {anecdote.votes} votes</div>
+  </div>
+)}
 
 const About = () => (
   <div>
@@ -134,8 +150,15 @@ const App = () => {
         <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
         <Route path="/about" render={() => <About />} />
         <Route path="/create" render={() => <CreateNew addNew={addNew} />} />
-        <Footer />
+        
+        <Route exact path="/anecdoteds" render={() =>
+          <AnecdoteList anecdotes={anecdotes} />
+        } />
+        <Route exact path="/anecdotes/:id" render={({ match }) =>
+          <Anecdote anecdote={anecdoteById(match.params.id)} />
+        } />
       </Router>
+        <Footer />
     </div>
   )
 }
