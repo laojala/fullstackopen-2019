586ab17d7746885e55f75da493d00014f1a2fdb8
Laura Ojala <laojala@gmail.com>
2020-01-01 10:30:42 +0200

    7.7: Users view. Users data contains currently removed blogs. Fix this.

--- chapter_5/bloglist-frontend/src/components/Users.js
+++ chapter_5/bloglist-frontend/src/components/Users.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Users = () => {
-   
-    return (
-        <>
-        <h2>Blogs</h2>
-        <div>User data</div>
+const Users = (props) => {
+
+    if (props.allUsers.length === 0)
+        return <div>loading users</div>
+
+    else
+        return (
+            <>
+                <h2>User data</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Blogs created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            {props.allUsers.map((user, index) => (
+                                <tr key={index}>
+                                    <td>
+                                    {user.name}
+                                    </td>
+                                    <td>
+                                    {user.blogs.length}
+                                    </td>
+                                </tr>
+                                ))}
+                   </tbody>
+                </table>
         </>
-    )
+        )
   }
 
-export default Users
+  const mapStateToProps = (state) => {
+    return {
+      allUsers: state.allUsers,
+    }
+  }
+  
+  const mapDispatchToProps = {
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users)
